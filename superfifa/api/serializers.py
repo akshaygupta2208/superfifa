cbc4a5a233770689701b4adc3008c2a002cdc39a
Akshay Kumar Gupta <akshaygupta@drishti-soft.com>
2017-06-14 01:30:03 +0530

    implemented child object in json

--- superfifa/api/serializers.py
+++ superfifa/api/serializers.py
@@ -1,9 +1,95 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from models import *
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email',
                   'first_name', 'last_name', 'is_active')
+
+
+class LeagueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = League
+        fields = ('id', 'name', 'rating', 'image_url')
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    league = LeagueSerializer(read_only=True)
+    country = serializers.ReadOnlyField(source='country.name')
+
+    class Meta:
+        model = Team
+        fields = ('id', 'name', 'rating', 'image_url',
+                  'country', 'league')
+
+
+class ChairmanSerializer(serializers.ModelSerializer):
+    team = TeamSerializer(read_only=True)
+
+    class Meta:
+        model = Chairman
+        fields = ('id', 'name', 'rating', 'image_url',
+                  'country', 'team')
+
+
+class ScoutSerializer(serializers.ModelSerializer):
+    country = serializers.ReadOnlyField(source='country.name')
+
+    class Meta:
+        model = Scout
+        fields = ('id', 'name', 'rating', 'image_url', 'country', 'hiring_fee')
+
+
+class CoachSerializer(serializers.ModelSerializer):
+    country = serializers.ReadOnlyField(source='country.name')
+
+    class Meta:
+        model = Coach
+        fields = ('id', 'name', 'type', 'reputation', 'flag_url',
+                  'image_url', 'country', 'wage')
+
+
+class OfficeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Office
+        fields = ('id', 'name', 'purchase_cost', 'running_cost',
+                  'player_capacity', 'level', 'rating', 'image_url')
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    current_club = TeamSerializer(read_only=True)
+    interested_club = TeamSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Player
+        fields = ('id', 'name', 'image_url', 'age', 'current_ability', 'potential_ability', 'overall_ability', 'performance',
+                  'happiness', 'agency', 'current_club', 'interested_club', 'current_contract', 'value', 'bonus', 'wins')
+
+
+class MediaCompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MediaCompany
+        fields = ('id', 'name')
+
+
+class JournalistSerializer(serializers.ModelSerializer):
+    company = MediaCompanySerializer(read_only=True)
+    country = serializers.ReadOnlyField(source='country.name')
+
+    class Meta:
+        model = Journalist
+        fields = ('id', 'name', 'country', 'company', 'image_url')
+
+
+class NewsSerializer(serializers.ModelSerializer):
+    journalist = JournalistSerializer(read_only=True)
+    player = serializers.ReadOnlyField(source='player.name')
+
+    class Meta:
+        model = News
+        fields = ('id', 'title', 'description',
+                  'news_type', 'journalist', 'player')
