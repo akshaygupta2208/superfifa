1d4e8e1f75d200644fb32903c4c12205ad5d65c6
Akshay Kumar Gupta <akshaygupta@drishti-soft.com>
2017-06-14 00:42:10 +0530

    Registered models on admin panel

--- superfifa/api/admin.py
+++ superfifa/api/admin.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from models import *
 
 # Register your models here.
+
+admin.site.register(League)
+admin.site.register(Team)
+admin.site.register(Chairman)
+admin.site.register(League)
+admin.site.register(Scout)
+admin.site.register(Coach)
+admin.site.register(Office)
+admin.site.register(Player)
+admin.site.register(MediaCompany)
+admin.site.register(Journalist)
+admin.site.register(News)
